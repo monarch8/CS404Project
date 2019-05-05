13735c582ca58d2193d297445f48e5efed23654c
Clarson1229 <27305718+clarson1229@users.noreply.github.com>
2019-05-05 15:46:48 -0500

    Revert "dictionaries"
    This reverts commit 7809fe1d936f32b07673e920e3fe04e733185812.

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
 import os
 
-courses = {303: 'Data Structures' , 490: 'Web Development', 371: 'Database', 550: 'Design and Analysis of Algorithms', 101: 'Python'}
-professors = {1: 'John Smith', 2: 'Samir Khan', 3: 'Ann Jackson', 4:'Lucas H', 5:'Dave Johns'}
-topics = {1: 'Programming', 2:'HTML', 3:'Math', 4: 'C++', 5:'Python', 6:'Stats',7:'Algorithms'}
