2b6344f532908375457869a7597a0b140c5a7c87
monarch8 <connorlm1997@gmail.com>
2019-05-05 16:05:51 -0500

    Course topic dictionary added

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import os
 
 courses = {303: 'Data Structures' , 490: 'Web Development', 371: 'Database', 550: 'Design and Analysis of Algorithms', 101: 'Python'}
 professors = {1: 'John Smith', 2: 'Samir Khan', 3: 'Ann Jackson', 4:'Lucas H', 5:'Dave Johns'}
-topics = {1: 'Programming', 2:'HTML', 3:'Math', 4: 'C++', 5:'Python', 6:'Stats',7:'Algorithms'}
+topics = {1: 'Programming', 2:'HTML', 3:'Math', 4: 'C++', 5:'Python', 6:'Stats', 7:'Algorithms'}
+
+courseTopic = {303: {1: 20, 2: 0, 3: 20, 4: 20, 5: 0, 6: 0, 7: 40}, 490: {1: 40, 2: 60, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0},
+               371: {1: 30, 2: 0, 3: 30, 4: 0, 5: 40, 6: 0, 7: 0}, 550: {1: 10, 2: 0, 3: 20, 4: 20, 5: 0, 6: 10, 7: 40},
+               101: {1: 25, 2: 0, 3: 15, 4: 0, 5: 40, 6: 0, 7: 20}}
+
+
