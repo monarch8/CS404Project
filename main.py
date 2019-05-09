4877d0743e5f33910402358bed7d044e3aac158e
Clarson1229 <27305718+clarson1229@users.noreply.github.com>
2019-05-09 11:12:10 -0500

    Formatting added

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import os
 
-courses = {303: 'Data Structures' , 490: 'Web Development', 371: 'Database', 550: 'Design and Analysis of Algorithms', 101: 'Python'}
-professors = {1: 'John Smith', 2: 'Samir Khan', 3: 'Ann Jackson', 4:'Lucas H', 5:'Dave Johns'}
-topics = {1: 'Programming', 2:'HTML', 3:'Math', 4: 'C++', 5:'Python', 6:'Stats', 7:'Algorithms'}
+# Course id: Course Name
+courses = {303: 'Data Structures' ,
+           490: 'Web Development',
+           371: 'Database',
+           550: 'Design and Analysis of Algorithms',
+           101: 'Python'}
+# Professor id: Professor Name
+professors = {1: 'John Smith',
+              2: 'Samir Khan',
+              3: 'Ann Jackson',
+              4:'Lucas H',
+              5:'Dave Johns'}
+# topic id: Topic Name
+topics = {1: 'Programming',
+          2:'HTML',
+          3:'Math',
+          4: 'C++',
+          5:'Python',
+          6:'Stats',
+          7:'Algorithms'}
 
 #Course id: {topic id: percent of course}
-courseTopic = {303: {1: 20, 2: 0, 3: 20, 4: 20, 5: 0, 6: 0, 7: 40}, 490: {1: 40, 2: 60, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0},
-               371: {1: 30, 2: 0, 3: 30, 4: 0, 5: 40, 6: 0, 7: 0}, 550: {1: 10, 2: 0, 3: 20, 4: 20, 5: 0, 6: 10, 7: 40},
+courseTopic = {303: {1: 20, 2: 0, 3: 20, 4: 20, 5: 0, 6: 0, 7: 40},
+               490: {1: 40, 2: 60, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0},
+               371: {1: 30, 2: 0, 3: 30, 4: 0, 5: 40, 6: 0, 7: 0},
+               550: {1: 10, 2: 0, 3: 20, 4: 20, 5: 0, 6: 10, 7: 40},
                101: {1: 25, 2: 0, 3: 15, 4: 0, 5: 40, 6: 0, 7: 20}}
 
 #Professor id: {topic id: knowledge of topic}
-expertiseTable = {1: {1: 5, 2: 1, 3: 2, 4: 5, 5: 5, 6: 1, 7: 1}, 2: {1: 3, 2: 1, 3: 3, 4: 5, 5: 2, 6: 4, 7: 5},
-                  3: {1: 2, 2: 5, 3: 1, 4: 1, 5: 3, 6: 1, 7: 1}, 4: {1: 5, 2: 2, 3: 2, 4: 2, 5: 5, 6: 5, 7: 3},
+expertiseTable = {1: {1: 5, 2: 1, 3: 2, 4: 5, 5: 5, 6: 1, 7: 1},
+                  2: {1: 3, 2: 1, 3: 3, 4: 5, 5: 2, 6: 4, 7: 5},
+                  3: {1: 2, 2: 5, 3: 1, 4: 1, 5: 3, 6: 1, 7: 1},
+                  4: {1: 5, 2: 2, 3: 2, 4: 2, 5: 5, 6: 5, 7: 3},
                   5: {1: 2, 2: 2, 3: 5, 4: 4, 5: 2, 6: 5, 7: 4}}
 
 def match(course, professor):
